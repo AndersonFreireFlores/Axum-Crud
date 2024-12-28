91abbd4becc0cd0825b4352b8cd30ed43730d839
AndersonFreireFlores <andersonfflores75@gmail.com>
2024-12-28 19:14:04 -0300

    dependencies, adn hello axum route for testing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
+use axum::Router;
+use axum::routing::get;
 
+#[tokio::main]
+async fn main() {
+
+    tracing_subscriber::fmt::init();
+
+    let app = Router::new()
+        .route("/hello", get(hello_axum));
+
+
+    let listener = tokio::net::TcpListener::bind
+        ("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello_axum() -> &'static str {
+    "Hello, Axum!"
 }
